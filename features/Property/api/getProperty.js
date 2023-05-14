da45336c24b04dfc1d0cce40cb2f154c55498518
Yotushka <serlight@ukr.net>
2023-05-15 00:19:28 +0300

    f

--- features/Property/api/getProperty.js
+++ features/Property/api/getProperty.js
@@ -1,40 +1,8 @@
 import { axios } from "@/lib/axios";
 
-const MAX_REQUESTS_PER_SECOND = 5;
-const REQUEST_INTERVAL_MS = 1000 / MAX_REQUESTS_PER_SECOND;
-const requestQueue = [];
-
 export const getProperty = async (id) => {
-    await enqueueRequest();
-
-    try {
-        const { data } = await axios.get("/properties/detail", {
-            params: { externalID: id },
-        });
-
-        return data;
-    } finally {
-        dequeueRequest();
-    }
-};
-
-function enqueueRequest() {
-    return new Promise((resolve) => {
-        const processRequest = () => {
-            if (requestQueue.length < MAX_REQUESTS_PER_SECOND) {
-                requestQueue.push(resolve);
-            } else {
-                setTimeout(processRequest, REQUEST_INTERVAL_MS);
-            }
-        };
-
-        processRequest();
+    const { data } = await axios.get("/properties/detail", {
+        params: { externalID: id }
     });
-}
-
-function dequeueRequest() {
-    if (requestQueue.length > 0) {
-        const resolve = requestQueue.shift();
-        resolve();
-    }
-}
+    return data;
+};
